35b752829763fe3ba2287e63095b458212eac3d0
mszekeres <szekeresm93@gmail.com>
2022-05-02 17:03:46 +0200

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,25 +4,25 @@ import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
-    public static Map<String, Object> fieldGetter(Object o, HashMap<String, Object> out, int count) {
+    public static Map<String, Object> fieldGetter(Object o, HashMap<String, Object> out, String prefix) {
         for (Field field : o.getClass().getDeclaredFields()) {
             field.setAccessible(true);
             String name = field.getName();
             Object value = null;
-            String s = String.valueOf(count);
             try {
                 value = field.get(o);
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
             }
+            String newPrefix = (prefix == null ? "" : prefix + ".") + name;
             if (!(value instanceof String) && value != null) {
-                count += 1;
-                fieldGetter(value, out, count);
+                fieldGetter(value, out, newPrefix);
             } else {
-                out.put("level." + s + "." + name, value);
+                out.put(newPrefix, value);
             }
         }
         return out;
@@ -42,21 +42,53 @@ public class Main {
             e.printStackTrace();
         }
 
-        int c = 0;
         HashMap<String, Object> outputOne = new HashMap<>();
-        fieldGetter(profileOne, outputOne, c);
+        fieldGetter(profileOne, outputOne, null);
 
-        c = 0;
         HashMap<String, Object> outputTwo = new HashMap<>();
-        fieldGetter(profileTwo, outputTwo, c);
+        fieldGetter(profileTwo, outputTwo, null);
 
         HashMap<String, Object> commonEntries = new HashMap<>();
+        ArrayList<String> differentEntries = new ArrayList<>();
+        ArrayList<String> notFoundEntriesOne = new ArrayList<>();
+        ArrayList<String> notFoundEntriesTwo = new ArrayList<>();
+
         for (String key : outputOne.keySet()) {
             if (outputTwo.containsKey(key) && outputTwo.get(key).equals(outputOne.get(key))) {
                 commonEntries.put(key, outputOne.get(key));
-                System.out.println("Matching entries: " + key + ": " + outputOne.get(key));
+            } else if (outputTwo.containsKey(key) && !outputTwo.get(key).equals(outputOne.get(key))) {
+                differentEntries.add(key);
+            } else {
+                notFoundEntriesOne.add(key);
             }
         }
-        System.out.println(commonEntries);
+
+        for (String key : outputTwo.keySet()) {
+            if (!outputOne.containsKey(key)) {
+                notFoundEntriesTwo.add(key);
+            }
+        }
+
+        Map<String, Object> commonEntriesTwo = outputOne.entrySet().stream()
+                .filter(x -> outputTwo.containsKey(x.getKey()))
+                .filter(x -> outputTwo.get(x.getKey()).equals(x.getValue()))
+                .collect(Collectors.toMap(x -> x.getKey(), x -> x.getValue()));
+
+        HashMap<String, Object> diffEntries = new HashMap<>();
+
+        for (String key : outputOne.keySet()) {
+            if (outputTwo.containsKey(key) && !outputTwo.get(key).equals(outputOne.get(key))) {
+                diffEntries.put(key, new ArrayList<>(Arrays.asList(outputOne.get(key), outputTwo.get(key))));
+            }
+        }
+
+        System.out.println(diffEntries);
+
+
+        System.out.println("Common entries: " + commonEntries);
+        System.out.println("Common entries via stream: " + commonEntriesTwo);
+        System.out.println("Different entries: " + differentEntries);
+        System.out.println("Entries not found in first profile: " + notFoundEntriesTwo);
+        System.out.println("Entries not found in second profile: " + notFoundEntriesOne);
     }
 }
---
+++ src/main/java/Output.java
@@ -0,0 +1,31 @@
+import java.util.Map;
+
+public class Output {
+    private Map<String, String[]> primitiveFields;
+    private Map<String, Output[]> beanFields;
+
+
+    public Map<String, String[]> getPrimitiveFields() {
+        return primitiveFields;
+    }
+
+    public void setPrimitiveFields(Map<String, String[]> primitiveFields) {
+        this.primitiveFields = primitiveFields;
+    }
+
+    public Map<String, Output[]> getBeanFields() {
+        return beanFields;
+    }
+
+    public void setBeanFields(Map<String, Output[]> beanFields) {
+        this.beanFields = beanFields;
+    }
+
+    @Override
+    public String toString() {
+        return "Output{" +
+                ", primitiveFields=" + primitiveFields +
+                ", beanFields=" + beanFields +
+                '}';
+    }
+}
