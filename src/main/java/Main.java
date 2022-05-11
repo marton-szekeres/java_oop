f0c76261307e87ebf3ea4ee0558453c6e75b1ade
mszekeres <szekeresm93@gmail.com>
2022-05-11 21:16:59 +0200

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -33,7 +33,6 @@ public class Main {
         Profile profileTwo = new Profile();
         File fileOne = new File("src/main/resources/profile_jane.json");
         File fileTwo = new File("src/main/resources/profile_john.json");
-        Output OutputOne = new Output();
         ObjectMapper mapper = new ObjectMapper();
 
         try {
@@ -49,49 +48,28 @@ public class Main {
         HashMap<String, Object> outputTwo = new HashMap<>();
         fieldGetter(profileTwo, outputTwo, null);
 
-        HashMap<String, Object> commonEntries = new HashMap<>();
-        ArrayList<String> differentEntries = new ArrayList<>();
-        ArrayList<String> notFoundEntriesOne = new ArrayList<>();
-        ArrayList<String> notFoundEntriesTwo = new ArrayList<>();
 
-        for (String key : outputOne.keySet()) {
-            if (outputTwo.containsKey(key) && outputTwo.get(key).equals(outputOne.get(key))) {
-                commonEntries.put(key, outputOne.get(key));
-            } else if (outputTwo.containsKey(key) && !outputTwo.get(key).equals(outputOne.get(key))) {
-                differentEntries.add(key);
-            } else {
-                notFoundEntriesOne.add(key);
+
+        Set<String> one = outputOne.keySet();
+        Set<String> two = outputTwo.keySet();
+        Set<String> union = new HashSet<String>(one);
+        union.addAll(two);
+
+        HashMap<String, Object> differentEntries = new HashMap<>();
+
+        for (String key : union) {
+            if (outputOne.containsKey(key) && outputTwo.containsKey(key) && !outputOne.get(key).equals(outputTwo.get(key))) {
+                Object[] temp = new Object[]{outputOne.get(key), outputTwo.get(key)};
+                differentEntries.put(key, temp);
+            } else if (!outputOne.containsKey(key)) {
+                Object[] temp = new Object[]{null, outputTwo.get(key)};
+                differentEntries.put(key, temp);
+            } else if (!outputTwo.containsKey(key)) {
+                Object[] temp = new Object[]{outputOne.get(key), null};
+                differentEntries.put(key, temp);
             }
         }
 
-        for (String key : outputTwo.keySet()) {
-            if (!outputOne.containsKey(key)) {
-                notFoundEntriesTwo.add(key);
-            }
-        }
-
-        Map<String, Object> commonEntriesTwo = outputOne.entrySet().stream()
-                .filter(x -> outputTwo.containsKey(x.getKey()))
-                .filter(x -> outputTwo.get(x.getKey()).equals(x.getValue()))
-                .collect(Collectors.toMap(x -> x.getKey(), x -> x.getValue()));
-
-        HashMap<String, Object> diffEntries = new HashMap<>();
-
-        for (String key : outputOne.keySet()) {
-            if (outputTwo.containsKey(key) && !outputTwo.get(key).equals(outputOne.get(key))) {
-                diffEntries.put(key, new ArrayList<>(Arrays.asList(outputOne.get(key), outputTwo.get(key))));
-            }
-        }
-
-        System.out.println(diffEntries);
-
-
-
-
-        System.out.println("Common entries: " + commonEntries);
-        System.out.println("Common entries via stream: " + commonEntriesTwo);
-        System.out.println("Different entries: " + differentEntries);
-        System.out.println("Entries not found in first profile: " + notFoundEntriesTwo);
-        System.out.println("Entries not found in second profile: " + notFoundEntriesOne);
+        System.out.println(union);
     }
 }
