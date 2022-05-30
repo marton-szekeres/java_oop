c14a2fe69ad2ac48655719f5dea5e001eb90a575
mszekeres <szekeresm93@gmail.com>
2022-05-30 15:21:25 +0200

    Initial commit

--- src/main/java/FirstNullNested.java
+++ src/main/java/FirstNullNested.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 public class FirstNullNested extends Comparison implements Strategy {
     public void compareAction(Param param) {
         if (param.getMapOne().get(param.getKey()) == null
-                && param.getMapOne().get(param.getKey()) instanceof String == false) {
+                && !(param.getMapOne().get(param.getKey()) instanceof String)) {
             param.getOutput().addBeanField(param.getKey(),
                     compareProfiles(null,
                             param.getMapper().convertValue(param.getMapTwo().get(param.getKey()), HashMap.class)));
