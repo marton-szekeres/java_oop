3141a0da2d08d11a438e4dd09c781378e7b2108a
mszekeres <szekeresm93@gmail.com>
2022-05-30 15:53:49 +0200

    Initial commit

--- src/main/java/Comparison.java
+++ src/main/java/Comparison.java
@@ -3,7 +3,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Comparison {
-    public static Output compareProfiles(HashMap<String, Object> inputMapOne, HashMap<String, Object> inputMapTwo) {
+    public Output compareProfiles(HashMap<String, Object> inputMapOne, HashMap<String, Object> inputMapTwo) {
         Param param = new Param();
         param.setMapOne(inputMapOne == null ? new HashMap<>() : inputMapOne);
         param.setMapTwo(inputMapTwo == null ? new HashMap<>() : inputMapTwo);
@@ -13,21 +13,14 @@ public class Comparison {
         Set<String> keySetunion = new HashSet<String>(keySetOne);
         keySetunion.addAll(keySetTwo);
 
-        DiffValuesString a = new DiffValuesString();
-        DiffValuesNested b = new DiffValuesNested();
-        FirstNullString c = new FirstNullString();
-        FirstNullNested d = new FirstNullNested();
-        SecondNullString e = new SecondNullString();
-        SecondNullNested f = new SecondNullNested();
-
         for (String key : keySetunion) {
             param.setKey(key);
-            a.compareAction(param);
-            b.compareAction(param);
-            c.compareAction(param);
-            d.compareAction(param);
-            e.compareAction(param);
-            f.compareAction(param);
+            new DiffValuesString().compareAction(param);
+            new DiffValuesNested().compareAction(param);
+            new FirstNullString().compareAction(param);
+            new FirstNullNested().compareAction(param);
+            new SecondNullString().compareAction(param);
+            new SecondNullNested().compareAction(param);
         }
         return param.getOutput();
     }
